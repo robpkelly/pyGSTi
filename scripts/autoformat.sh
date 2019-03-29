246ecd8306093db9fa372b566e90c87edcc31ab1
Rob Kelly <rpkelly@sandia.gov>
2019-03-29 13:46:43 -0600

    Better argument semantics for autoformatter

--- scripts/autoformat.sh
+++ scripts/autoformat.sh
@@ -5,19 +5,27 @@ PYGSTI=$(realpath $(dirname $(realpath $0))/..)
 
 AUTOPEP8_BIN=$(which autopep8)
 AUTOPEP8_BASIC_ARGS="--verbose --recursive --in-place"
-AUTOPEP8_EXTRA_ARGS="--aggressive --select=W291"
+AUTOPEP8_EXTRA_ARGS="--aggressive --select=W2"
 
 if [ $? -ne 0 ]; then
     echo "No `autopep8` in path. Try running `pip install autopep8`."
     exit 1
 fi
 
-# General whitespace autoformatting
-FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $@"
-echo "\$ $FORMAT_CMD"
-exec $FORMAT_CMD
+if [ $# -eq 0 ]; then
+    TARGET="$PYGSTI"
+else
+    TARGET="$@"
+fi
 
-# Remove trailing whitespace (requires --aggressive flag, which can potentially change semantics)
-FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $AUTOPEP8_EXTRA_ARGS $@"
+# General whitespace autoformatting
+FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $TARGET"
 echo "\$ $FORMAT_CMD"
-exec $FORMAT_CMD
+$FORMAT_CMD
+
+# Remove trailing whitespace, which requires --aggressive flag
+# In principle, this can potentially alter semantics, but as long as
+# we select only W2 warnings this is practically impossible.
+FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $AUTOPEP8_EXTRA_ARGS $TARGET"
+echo "\$ $FORMAT_CMD"
+$FORMAT_CMD
