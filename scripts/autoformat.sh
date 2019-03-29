3a7dcf8bc6ea4dc96ce23c91f137d545e79fe05f
Rob Kelly <rpkelly@sandia.gov>
2019-03-29 14:36:46 -0600

    Autoformatter documentation

--- scripts/autoformat.sh
+++ scripts/autoformat.sh
@@ -1,5 +1,21 @@
 #! /usr/bin/env bash
-# Run code formatting tools across the codebase
+# Run code formatting tools across the codebase.
+#
+# About:
+# This script makes two passes with autopep8.
+# The first pass makes "nonaggressive" changes -- formatting changes
+# which couldn't possibly introduce semantic differences.
+# The second pass makes aggressive corrections for the following
+# formatting errors:
+#
+# - W291: trailing whitespace
+# - W292: no newline at end of file
+# - W293: blank line contains whitespace
+#
+# In principle, these can potentially alter semantics. In practice,
+# though, if this could possibly change a unit's semantics, that unit
+# should probably be revised anyway. Still, it's not a bad idea to
+# manually check over autoformatted files before committing.
 
 PYGSTI=$(realpath $(dirname $(realpath $0))/..)
 
@@ -24,8 +40,6 @@ echo "\$ $FORMAT_CMD"
 $FORMAT_CMD
 
 # Remove trailing whitespace, which requires --aggressive flag
-# In principle, this can potentially alter semantics, but as long as
-# we select only W2 warnings this is practically impossible.
 FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $AUTOPEP8_EXTRA_ARGS $TARGET"
 echo "\$ $FORMAT_CMD"
 $FORMAT_CMD
