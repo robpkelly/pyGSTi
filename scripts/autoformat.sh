c456862eb20a97d5800e58d7861c79074ddf8590
Rob Kelly <rpkelly@sandia.gov>
2019-03-29 13:25:51 -0600

    autoformat script deletes trailing whitespace

--- scripts/autoformat.sh
+++ scripts/autoformat.sh
@@ -2,21 +2,22 @@
 # Run code formatting tools across the codebase
 
 PYGSTI=$(realpath $(dirname $(realpath $0))/..)
+
 AUTOPEP8_BIN=$(which autopep8)
+AUTOPEP8_BASIC_ARGS="--verbose --recursive --in-place"
+AUTOPEP8_EXTRA_ARGS="--aggressive --select=W291"
 
 if [ $? -ne 0 ]; then
     echo "No `autopep8` in path. Try running `pip install autopep8`."
     exit 1
 fi
 
-if [ $# -eq 0 ]; then
-    # Default source targets
-    TARGET_SRC=("$PYGSTI/packages/pygsti" "$PYGSTI/test" "$PYGSTI/scripts" "$PYGSTI/setup.py")
-else
-    # Use the supplied argument, if given
-    TARGET_SRC=$@
-fi
+# General whitespace autoformatting
+FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $@"
+echo "\$ $FORMAT_CMD"
+exec $FORMAT_CMD
 
-FORMAT_CMD="$AUTOPEP8_BIN --aggressive --experimental --verbose --recursive --in-place ${TARGET_SRC[*]}"
+# Remove trailing whitespace (requires --aggressive flag, which can potentially change semantics)
+FORMAT_CMD="$AUTOPEP8_BIN $AUTOPEP8_BASIC_ARGS $AUTOPEP8_EXTRA_ARGS $@"
 echo "\$ $FORMAT_CMD"
 exec $FORMAT_CMD
